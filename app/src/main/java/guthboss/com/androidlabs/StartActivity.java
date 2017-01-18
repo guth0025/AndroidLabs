1e40885208b75dbcf1561c800aa6ec286f540c5c
NOAH\guthr_000 <guthboss@gmail.com>
2017-01-18 15:44:28 -0500

    Everything but: -Switch all text to string xml -add second language option for all text

--- app/src/main/java/guthboss/com/androidlabs/StartActivity.java
+++ app/src/main/java/guthboss/com/androidlabs/StartActivity.java
@@ -2,6 +2,9 @@ package guthboss.com.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.d("Switch","State Changed");
+
+            }
+        });
     }
+
+
 }
